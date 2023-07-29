9e359f55013f82b85ac06d5e8a57e07b4e7c238e
Tereza Smolová <framboiseart@email.cz>
2023-07-29 18:09:33 +0200

    Add Eleventy

---
+++ .eleventy.js
@@ -0,0 +1,55 @@
+module.exports = function(eleventyConfig) {
+    // Výchozí výstupní složka je: _site
+    // Zkopírovat images/ do _site/images
+    eleventyConfig.addPassthroughCopy("images");
+    // Zkopírovat css/ to _site/css/
+    eleventyConfig.addPassthroughCopy("css");
+
+    eleventyConfig.addPassthroughCopy("icons");
+
+    eleventyConfig.addPassthroughCopy("flavicon")
+
+    eleventyConfig.addPassthroughCopy("scripts")
+
+    // Přidání passthrough pro soubory .txt
+    eleventyConfig.addPassthroughCopy("*.txt");
+
+    // Přidání passthrough pro soubory .xml
+    eleventyConfig.addPassthroughCopy("*.xml");
+
+    // Přidání passthrough pro soubory .js
+    eleventyConfig.addPassthroughCopy("*.js");
+
+    eleventyConfig.setBrowserSyncConfig({
+      callbacks: {
+        ready: function (err, browserSync) {
+          const mime = browserSync.mime;
+          mime.define({
+            'text/javascript': ['js']
+          });
+        }
+      }
+    });
+
+    return async () => {
+      let output = await esbuild.build({
+        target: 'es2020',
+        entryPoints: [path],
+        minify: true,
+        bundle: true,
+        write: false,
+      });
+    
+      return output.outputFiles[0].text;
+    }
+
+
+    return {
+        // možné formáty šablon
+        templateFormats: ["njk", "html", "md"],
+        // jako šablonovací jazyk zvolíme Nunjucks`
+        markdownTemplateEngine: "njk",
+        htmlTemplateEngine: "njk",
+        dataTemplateEngine: "njk",
+      }    
+   };
